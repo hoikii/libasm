b8ca9ebe366dae0bc954ba4e8402c5ada5ad509a
kanlee <kanlee@student.42seoul.kr>
2021-04-09 22:41:29 +0900

    protect when input is NULL

--- ft_strlen.s
+++ ft_strlen.s
@@ -1,8 +1,22 @@
+; ------------------------------------
+;	size_t ft_strlen(const char *s)
+;	{
+;		if (s == NULL)
+;			return ;
+;		int i = 0;
+;		while (s[i] != 0)
+;			i++;
+;		return i;
+;	}
+; -------------------------------------
+
 section .text
 	global _ft_strlen
 
 _ft_strlen:
 	mov	rax, 0						; i = 0
+	cmp	rdi, 0
+	je	done
 loop:
 		cmp	BYTE [rdi + rax], 0		; *(str + i) == 0
 		je	done
